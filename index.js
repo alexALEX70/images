70309726085cdb6037ba23728cdd413f2b86ffe3
alexALEX70 <alexALEX19701703@gmail.com>
2021-11-02 18:42:28 +0300

    FizzBuzz

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello, alex')
+function func(start, end) {
+
+    for (let i = start; i <= end; i++) {
+        let res = '';
+
+        if (i % 3 == 0)
+            res += 'Fizz';
+
+        if (i % 5 == 0)
+            res += 'Buzz';
+
+        console.log(res || i);
+    }
+}
+func(1, 50);
